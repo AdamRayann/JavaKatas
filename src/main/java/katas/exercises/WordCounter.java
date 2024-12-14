028d31497b900d5a398d75ff76ca5db25f24b965
AdamRayann <129179113+AdamRayann@users.noreply.github.com>
2024-12-14 17:11:17 +0200

    countWords done

--- src/main/java/katas/exercises/WordCounter.java
+++ src/main/java/katas/exercises/WordCounter.java
@@ -8,8 +8,19 @@ public class WordCounter {
      * @param sentence the input string (a sentence)
      * @return the number of words in the sentence
      */
-    public static int countWords(String sentence) {
-        return 0;
+    public static int countWords(String sentence)
+    {
+        if(sentence.isEmpty())
+            return 0;
+        int cnt=1;
+        for (char c : sentence.toCharArray())
+        {
+            if(c==' ')
+            {
+                cnt+=1;
+            }
+        }
+        return cnt;
     }
 
     public static void main(String[] args) {
