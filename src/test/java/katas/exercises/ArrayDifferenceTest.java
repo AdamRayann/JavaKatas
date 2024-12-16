7f5cd16cd2ba59eafac2846a61fc91bf1a4ccafc
AdamRayann <129179113+AdamRayann@users.noreply.github.com>
2024-12-16 21:22:22 +0200

    tests done

--- src/test/java/katas/exercises/ArrayDifferenceTest.java
+++ src/test/java/katas/exercises/ArrayDifferenceTest.java
@@ -3,7 +3,6 @@ package katas.exercises;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import katas.exercises.ArrayDifference.*;
 public class ArrayDifferenceTest {
 
     int[] testArray= {6, 500, 3, -3, 0, 6, -60};
