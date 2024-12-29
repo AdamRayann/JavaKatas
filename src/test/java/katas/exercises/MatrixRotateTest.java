8e04994682cf98f4b2a35702b17c89afe3dc0bf9
AdamRayann <129179113+AdamRayann@users.noreply.github.com>
2024-12-29 21:10:38 +0200

    test done

---
+++ src/test/java/katas/exercises/MatrixRotateTest.java
@@ -0,0 +1,15 @@
+package katas.exercises;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatrixRotateTest {
+
+    @Test
+    @DisplayName("valid ")
+    public void test1() {
+        assertEquals(true, true);
+    }
+}
